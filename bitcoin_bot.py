5ac9422f98b81c9e3f4a86122ebda336476f6839
SDR-mania2 <55984656+SDR-mania2@users.noreply.github.com>
2019-11-07 18:08:36 +0900

    Update bitcoin_bot.py

--- bitcoin_bot.py
+++ bitcoin_bot.py
@@ -6,9 +6,8 @@ def main():
   #板情報を取得。本来API keyが必要だが、板情報取得だけなら不要。
   api = pybitflyer.API()
   
-  #本来はこのように指定する。
-  api = pybitflyer.API(api_key="...", api_secret="...")
-
+  #本来は以下のように指定する。
+  #api = pybitflyer.API(api_key="...", api_secret="...")
 
   #BTC_JPYを指定して取得。
   board = api.board(product_code="BTC_JPY")
@@ -21,7 +20,3 @@ if __name__=='__main__':
 
 #今後チャート作成や取引部分も実装予定。
 #というか動くのかこれ…
-
-
-
-
